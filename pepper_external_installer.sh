4718dec82398e9a80e309a6e3e6fac7863d54a98
Rodrigo Muñoz <rorro.mr@gmail.com>
2017-01-20 14:55:03 -0300

    Meshes install

--- pepper_external_installer.sh
+++ pepper_external_installer.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 mkdir -p pepper_ws/src
-wget -O .rosinstall
+cd pepper_ws/src
+catkin_init_workspace
+wget https://raw.githubusercontent.com/uchile-robotics/pepper_system/master/pepper_external.rosinstall -O .rosinstall
 rosws update
 cd ..
 catkin_make
+echo "source ~/pepper_ws/devel/setup.bash" >> ~/.bashrc
+
+# Install Pepper meshes on pepper_ws/src/pepper_meshes
+./devel/tmp/installer.run
+
+
+
