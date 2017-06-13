88d53c0fdc3d3086475e7c08b22dd400630ed012
Diego Baño <diego.bano@ug.uchile.cl>
2017-06-13 19:35:51 -0400

    Currently installing llvm

--- nav_install_script.sh
+++ nav_install_script.sh
@@ -14,6 +14,8 @@ make
 sudo make install
 
 
+
+
 # hdf5 1.8.12
 # ---------------------------------------------------------
 # notes:
@@ -156,19 +158,292 @@ cd presentproto-1.1/
 make
 sudo make install
 
+################################# TODO OK HASTA ACA ##################################
 
-# TODO OK HASTA ACÁ
-###########################################################################
-
-
+# xproto 7.0.31
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
 cd ~/pepper_dep
-wget https://www.x.org/archive//individual/lib/libX11-1.5.0.tar.gz -O libX11-1.5.0.tar.gz --no-check-certificate
-tar -xvzf libX11-1.5.0.tar.gz
-cd libX11-1.5.0/
+wget https://www.x.org/archive/individual/proto/xproto-7.0.31.tar.gz -O xproto-7.0.31.tar.gz --no-check-certificate
+tar -xvzf xproto-7.0.31.tar.gz
+cd xproto-7.0.31/
 ./configure
 make
 sudo make install
 
+# xextproto 7.3.0
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive/individual/proto/xextproto-7.3.0.tar.gz -O xextproto-7.3.0.tar.gz --no-check-certificate
+tar -xvzf xextproto-7.3.0.tar.gz
+cd xextproto-7.3.0/
+./configure
+make
+sudo make install
+
+# xtrans 1.3.5
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/xtrans-1.3.5.tar.gz -O xtrans-1.3.5.tar.gz --no-check-certificate
+tar -xvzf xtrans-1.3.5.tar.gz
+cd xtrans-1.3.5/
+./configure
+make
+sudo make install
+
+# xcb-proto 1.12
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://xcb.freedesktop.org/dist/xcb-proto-1.12.tar.gz -O xcb-proto-1.12.tar.gz --no-check-certificate
+tar -xvzf xcb-proto-1.12.tar.gz
+cd xcb-proto-1.12/
+./configure
+make
+sudo make install
+
+# pthread-stubs 0.4
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://xcb.freedesktop.org/dist/libpthread-stubs-0.4.tar.gz -O libpthread-stubs-0.4.tar.gz --no-check-certificate
+tar -xvzf libpthread-stubs-0.4.tar.gz
+cd libpthread-stubs-0.4/
+./configure
+make
+sudo make install
+
+# xau 1.0.8
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libXau-1.0.8.tar.gz -O libXau-1.0.8.tar.gz --no-check-certificate
+tar -xvzf libXau-1.0.8.tar.gz
+cd libXau-1.0.8/
+./configure
+make
+sudo make install
+
+# xcb 1.12
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - xcb-proto
+# - xau
+# - pthread-stubs
+cd ~/pepper_dep
+wget https://xcb.freedesktop.org/dist/libxcb-1.12.tar.gz -O libxcb-1.12.tar.gz --no-check-certificate
+tar -xvzf libxcb-1.12.tar.gz
+cd libxcb-1.12/
+./configure
+make
+sudo make install
+
+# kbproto 1.0.7
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive/individual/proto/kbproto-1.0.7.tar.gz -O kbproto-1.0.7.tar.gz --no-check-certificate
+tar -xvzf kbproto-1.0.7.tar.gz
+cd kbproto-1.0.7/
+./configure
+make
+sudo make install
+
+# inputproto 2.3
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive/individual/proto/inputproto-2.3.tar.gz -O inputproto-2.3.tar.gz --no-check-certificate
+tar -xvzf inputproto-2.3.tar.gz
+cd inputproto-2.3/
+./configure
+make
+sudo make install
+
+# xsmhfence 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - xtrans
+# - xcb
+# - kbproto
+# - inputproto
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libX11-1.6.5.tar.gz -O libX11-1.6.5.tar.gz --no-check-certificate
+tar -xvzf libX11-1.6.5.tar.gz
+cd libX11-1.6.5/
+./configure
+make
+sudo make install
+
+# xsmhfence 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - xproto
+# - xextproto
+# - x11
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libXext-1.3.3.tar.gz -O libXext-1.3.3.tar.gz --no-check-certificate
+tar -xvzf libXext-1.3.3.tar.gz
+cd libXext-1.3.3/
+./configure
+make
+sudo make install
+
+# damageproto 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive/individual/proto/damageproto-1.2.1.tar.gz -O damageproto-1.2.1.tar.gz --no-check-certificate
+tar -xvzf damageproto-1.2.1.tar.gz
+cd damageproto-1.2.1/
+./configure
+make
+sudo make install
+
+# fixesproto 5.0
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive/individual/proto/fixesproto-5.0.tar.gz -O fixesproto-5.0.tar.gz --no-check-certificate
+tar -xvzf fixesproto-5.0.tar.gz
+cd fixesproto-5.0/
+./configure
+make
+sudo make install
+
+# xsmhfence 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - fixesproto
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libXfixes-5.0.tar.gz -O libXfixes-5.0.tar.gz --no-check-certificate
+tar -xvzf libXfixes-5.0.tar.gz
+cd libXfixes-5.0/
+./configure
+make
+sudo make install
+
+# xsmhfence 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - damageproto
+# - xfixes
+# - fixesproto
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libXdamage-1.1.tar.gz -O libXdamage-1.1.tar.gz --no-check-certificate
+tar -xvzf libXdamage-1.1.tar.gz
+cd libXdamage-1.1/
+./configure
+make
+sudo make install
+
+# xsmhfence 1.2
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://www.x.org/archive//individual/lib/libxshmfence-1.2.tar.gz -O libxshmfence-1.2.tar.gz --no-check-certificate
+tar -xvzf libxshmfence-1.2.tar.gz
+cd libxshmfence-1.2/
+./configure
+make
+sudo make install
+
+######################### TODO OK HASTA ACA? #########################
+
+# gcc 4.8.5
+# ---------------------------------------------------------
+# notes:
+# - This is the latest of the supported versions for LLVM
+# - This takes a loooooooong time to compile (and a little time to download)
+# - IT IS NECESSARY TO RESTART FOR CHANGES TO TAKE PLACE
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz -O cmake-3.8.2.tar.gz --no-check-certificate
+tar -xvzf cmake-3.8.2.tar.gz
+cd cmake-3.8.2/
+./configure
+gmake
+make
+sudo make install
+
+# gcc 4.8.5
+# ---------------------------------------------------------
+# notes:
+# - This is the latest of the supported versions for LLVM
+# - This takes a loooooooong time to compile (and a little time to download)
+# - IT IS NECESSARY TO RESTART FOR CHANGES TO TAKE PLACE
+# prerequisites:
+# - None
+cd ~/pepper_dep
+wget ftp://ftp.gnu.org/gnu/gcc/gcc-4.8.5/gcc-4.8.5.tar.gz -O gcc-4.8.5.tar.gz --no-check-certificate
+tar -xvzf gcc-4.8.5.tar.gz
+cd gcc-4.8.5/
+./configure
+gmake
+make
+sudo make install
+
+# llvm 4.0.0
+# ---------------------------------------------------------
+# notes:
+# - This is the latest version
+# prerequisites:
+# - gcc >= 4.8
+cd ~/pepper_dep
+svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+cd llvm/tools
+svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+cd ../..
+mkdir build-llvm
+cd build-llvm
+cmake -G -DCMAKE_C_COMPILER=$(which gcc) "Unix Makefiles" ../llvm
+make
 
 # mesa 10.1.3
 # ---------------------------------------------------------
@@ -177,6 +452,7 @@ sudo make install
 # prerequisites:
 # - libxml2 for python2
 # - glproto >= 1.4.14
+# - many libx libraries
 cd ~/pepper_dep
 wget https://mesa.freedesktop.org/archive/older-versions/10.x/10.1.3/MesaLib-10.1.3.tar.gz -O MesaLib-10.1.3.tar.gz --no-check-certificate
 tar -xvzf MesaLib-10.1.3.tar.gz
